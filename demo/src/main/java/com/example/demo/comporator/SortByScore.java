9ba4b1687a9cadd323ba563f01afccac0f545691
kunavich <48840136+kunavich@users.noreply.github.com>
2024-04-03 17:59:01 +0300

    Create compotator

---
+++ demo/src/main/java/com/example/demo/comporator/SortByScore.java
@@ -0,0 +1,12 @@
+package com.example.demo.comporator;
+
+import com.example.demo.entity.Match;
+
+import java.util.Comparator;
+
+public class SortByScore implements Comparator<Match> {
+    public int compare(Match a, Match b)
+    {
+        return a.getAwayNameScore() + a.getHomeNameScore() - b.getHomeNameScore() - b.getAwayNameScore();
+    }
+}
