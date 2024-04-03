a94f55d9579106b817a3a081cdfd362a3c0d1ca0
kunavich <48840136+kunavich@users.noreply.github.com>
2024-04-03 14:17:38 +0300

    create entity

---
+++ demo/src/main/java/com/example/demo/entity/Match.java
@@ -0,0 +1,64 @@
+package com.example.demo.entity;
+
+public class Match {
+
+    private String homeName;
+    private String awayName;
+
+    private int homeNameScore;
+    private int awayNameScore;
+
+    public Match(String homeName, String awayName) {
+        this.homeName = homeName;
+        this.awayName = awayName;
+
+        this.awayNameScore = 0;
+        this.homeNameScore = 0;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Match{" +
+                "homeName='" + homeName + '\'' +
+                ", awayName='" + awayName + '\'' +
+                ", homeNameScore=" + homeNameScore +
+                ", awayNameScore=" + awayNameScore +
+                '}';
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Match match = (Match) object;
+        return homeNameScore == match.homeNameScore && awayNameScore == match.awayNameScore && homeName.equals(match.homeName) && awayName.equals(match.awayName);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), homeName, awayName, homeNameScore, awayNameScore);
+    }
+
+    public String getHomeName() {
+        return homeName;
+    }
+
+    public String getAwayName() {
+        return awayName;
+    }
+
+    public int getHomeNameScore() {
+        return homeNameScore;
+    }
+
+    public void setHomeNameScore(int homeNameScore) {
+        this.homeNameScore = homeNameScore;
+    }
+
+    public int getAwayNameScore() {
+        return awayNameScore;
+    }
+
+    public void setAwayNameScore(int awayNameScore) {
+        this.awayNameScore = awayNameScore;
+    }
+}
